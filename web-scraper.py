cc09d4446083f4efb670865e248cb0b2a3f8d010
Michael Crombie <mcrombie1994@gmail.com>
2020-02-21 09:49:57 -0600

    Updating python scraper

--- web-scraper.py
+++ web-scraper.py
@@ -3,10 +3,13 @@ print('Using python again...hurray!')
 # https://towardsdatascience.com/how-to-web-scrape-with-python-in-4-minutes-bc49186a8460
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 # https://code.tutsplus.com/tutorials/how-to-read-and-write-csv-files-in-python--cms-29907
+# https://stackoverflow.com/questions/27092833/unicodeencodeerror-charmap-codec-cant-encode-characters
 
 import requests
 import urllib.request
 import time
+import csv
+import io
 from bs4 import BeautifulSoup
 
 url = 'https://en.wikipedia.org/wiki/List_of_cities_of_the_ancient_Near_East'
@@ -15,19 +18,22 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 tags = soup.findAll('a')
 
-# link = one_a_tag['href']
-
-
-arr = []
-for tag in tags:
-    link = tag.get('title')
-    if(link != None):
-        print(tag.get_text())
-    # print('/n')
-    # arr.append(tag)
+with io.open('example.csv', "w", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    arr = []
+    for tag in tags:
+        link = tag.get('title')
+        # REMOVE UNKNOWN CHARACTERS 
+        print(link)
+        if(link != None):
+            writer.writerow([link])
+            print(link)
     # time.sleep(1) #pause the code for a sec
+    # writer.writerow(arr)
 
+# with open('example.csv','rw') as fileLineCleaner:
+#     for line in fileLineCleaner:
+#         if not line.isspace():
+#             fileLineCleaner.write(line)
 
- 
-
-# print(arr)
+print('Completed file writing!')
